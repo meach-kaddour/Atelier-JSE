7ff70d626d62ebb66fa813ce712dbf316adaff86
meach <meachkaddour1@gmail.com>
2020-12-03 11:52:39 +0100

    complete boucle exercice

---
+++ javaProject/boucle2/src/boucle2/question2.java
@@ -0,0 +1,45 @@
+package boucle2;
+
+public class question2 {
+	public static void main(String[] args) {
+
+		 //Question 2
+		for (int i = 1; i <= 30; i++){
+			   for (int j = 1; j <= 100; j++){
+			      System.out.print(i * j + " ");
+			   }
+			   System.out.println();
+			}
+
+		// Question 3
+		//for (double i = 1; i <= 10; i +=i/2) {
+		//	System.out.println(i);
+		//}
+
+		// Question 4
+		//for (int i = 1; i <=15; i++) {
+
+		 //System.out.println("on y arrive presque "+i+" pas");
+		//}
+
+		// Question 5
+		 //for (int i =20; i>=0; i--) {
+
+		 //System.out.println("c'est presque bon .. "+i+" pas");
+		 //}
+
+		// Question 6
+		//for (int i = 1; i <=100; i+=15) {
+
+		//System.out.println("On tient le bon bout... "+i+" pas");
+		//}
+
+		// Question 7
+
+		/* for (int i = 200; i >=0; i = i - 12) {
+
+		 System.out.println("Enfin " + i + " pas");
+		 }*/
+
+	}
+}
---
+++ javaProject/boucle2/src/module-info.java
@@ -0,0 +1,2 @@
+module boucle2 {
+}
