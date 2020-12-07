235eae27c4f4ed644c9f5d926e9864aa054d38df
meach <meachkaddour1@gmail.com>
2020-12-07 19:16:49 +0100

    ajoute la solution d'exercice de tva comme demandés

--- javaProject/variable2/src/variable2/question2.java
+++ javaProject/variable2/src/variable2/question2.java
@@ -3,27 +3,36 @@ package variable2;
 import java.util.Scanner;
 
 public class question2 {
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		double TTC, Remise = 0, TVA = 0;
+
+		double TTC, Remise, TVA;
+	
 		System.out.println("Tapez le prix HT");
 		int HT = input.nextInt();
-		
-		if (HT>1000) {
-			Remise = HT * 0.1;
-			TVA = (HT - Remise) * 0.2;
-			TTC = (HT - Remise) + TVA;
-			System.out.println("le montant de remise est "+Remise+" MAD");
-			System.out.println("le montantant de TVA "+TVA+" MAD");
-			System.out.println("le montant TTC avec Remise "+TTC+" MAD");
-		}
-		else {
-			TVA = HT * 0.2;
-			TTC = HT+ TVA;
-			System.out.println("Désoli vous n'avez pas le remise");
-			System.out.println("le montantant de TVA "+TVA+" MAD");
-			System.out.println("le montant TTC est "+TTC+" MAD");
-			
+
+		if (HT >= 200) {
+			System.out.println("vous voulez du remise");
+			 String reponse = input.next();
+
+			if (reponse.equalsIgnoreCase("oui")) {
+
+				Remise = (int)(HT * 0.1);
+				TVA = (int)((HT - Remise) * 0.2);
+				TTC = (int)((HT - Remise) + TVA);
+				System.out.println("le montant de remise est " + Remise + " MAD");
+				System.out.println("le montantant de TVA " + TVA + " MAD");
+				System.out.println("le montant TTC avec Remise " + TTC + " MAD");
+			}
+			if (reponse.equalsIgnoreCase("non")) {
+				TVA = HT * 0.2;
+				TTC = HT + TVA;
+
+				System.out.println("le montantant de TVA " + TVA + " MAD");
+				System.out.println("le montant TTC est " + TTC + " MAD");
+
+			}
 		}
 
 	}
